e9bb57991596ced9e2c31381f5585096a7e6efe6
BIT Student <classroom@bgit.rs>
2020-02-12 18:08:33 +0100

    switch exercises

---
+++ switch.js
@@ -0,0 +1,49 @@
+/*var a = "K";
+var result = "";
+switch (a) {
+    case "A":
+        result = "Good Job";
+        break;
+    case "B":
+        result = "Pretty good";
+        break;
+    case "C":
+        result = "Passed";
+        break;
+    case "D":
+        result = "NOt so good";
+        break;
+    case "F":
+        result = "Failed"
+
+        break;
+    default:
+        result = "Unknown grade"
+        break;
+
+
+
+} console.log(result);*/
+
+
+
+
+
+var nun = [2, -4, 5, -2, -11];
+if (nun[0] < 0) {
+    console.log(nun[0]);
+}
+
+if (nun[1] < 0) {
+    console.log(nun[1]);
+}
+if (nun[2] < 0) {
+
+    console.log(nun[2]);
+}
+
+
+
+
+
+
